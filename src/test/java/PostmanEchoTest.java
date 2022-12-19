1f1afc43efac72030ad469b099324a58825fd89f
VikaMin <4673813@gmail.com>
2022-12-19 18:25:49 +0300

    first commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,27 @@
+import io.restassured.mapper.ObjectMapperType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+
+
+
+class PostmanEchoTest {
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some"))
+        ;
+    }
+}
